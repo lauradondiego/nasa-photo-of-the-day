5d12c852a0166ed8bfca74132447ccff395901a0
Laura Don Diego <lauradondiego@Leonards-Air.home>
2019-07-17 17:27:25 -0400

    I got the photo and header and date to display all on the app.js file, now I am going to create other components where I can export and import them in

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [image, setImage] = useState();
+  const [date, setDate] = useState();
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2019-07-15`
+      )
+      .then(response => {
+        setImage(response.data.hdurl);
+        setDate(response.data.date);
+
+        console.log(response.data);
+      });
+  }, []);
+  // the [] means you only want it it to load once
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <h1>Nasa Photo of the Day</h1>
+      <h2>{date}</h2>
+      <img src={image} alt="planet image" /> 
+      {/* javascript image needs curly */}
     </div>
   );
 }
