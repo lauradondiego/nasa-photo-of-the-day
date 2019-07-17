62246c48d07148ede44336a6c34acefc12305a04
Laura Don Diego <lauradondiego@Leonards-Air.home>
2019-07-17 19:15:26 -0400

    completed today's project, created another component with an axios get request using useEffect and passed that info to my app.js file

--- src/App.js
+++ src/App.js
@@ -1,31 +1,16 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import "./App.css";
+import NasaData from "./Components/NasaData";
 
 function App() {
-  const [image, setImage] = useState();
-  const [date, setDate] = useState();
-
-  useEffect(() => {
-    axios
-      .get(
-        `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2019-07-15`
-      )
-      .then(response => {
-        setImage(response.data.hdurl);
-        setDate(response.data.date);
-
-        console.log(response.data);
-      });
-  }, []);
-  // the [] means you only want it it to load once
-
   return (
-    <div className="App">
-      <h1>Nasa Photo of the Day</h1>
-      <h2>{date}</h2>
-      <img src={image} alt="planet image" /> 
-      {/* javascript image needs curly */}
+    <div className="container">
+      <div className="header">
+        <h1>Nasa Photo of the Day</h1>
+      </div>
+      <div className="NasaData">
+        <NasaData />
+      </div>
     </div>
   );
 }
---
+++ src/Components/NasaData.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "../App.css";
+
+const NasaData = () => {
+  const [image, setImage] = useState();
+  const [date, setDate] = useState();
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2019-07-15`
+      )
+      .then(response => {
+        setImage(response.data.hdurl);
+        setDate(response.data.date);
+
+        console.log(response.data);
+      });
+  }, []);
+  // the [] means you only want it it to load once
+
+  return (
+    <div className="App">
+      <h2>{date}</h2>
+      {/* we defined date in line 7 */}
+      <img src={image} style={{ maxWidth: "400px" }} alt="planet image" />
+      {/* javascript image needs curly */}
+    </div>
+  );
+};
+export default NasaData;
